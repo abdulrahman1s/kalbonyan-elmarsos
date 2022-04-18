1f431e347ea5e2474b27d8c97b5dcada8591266c
abdulrahman1s <61483023+abdulrahman1s@users.noreply.github.com>
2022-04-18 12:13:29 +0000

    Add challenges of Fundamentals

---
+++ foundations/fundamentals/1-whats-the-output.py
@@ -0,0 +1,18 @@
+print("Challange 1:")
+
+message = "This is going to be tricky ;)"
+
+Message = "Very tricky"
+
+print(message) # This is going to be tricky ;)
+
+result = 2**3
+
+print("2**3 =", result) # 2**3 = 8
+
+result = 5 - 3
+
+print("5 - 3 =", result) # 5 - 3 = 2
+
+
+print("Challenge complete!")
---
+++ foundations/fundamentals/2-guessing-game.py
@@ -0,0 +1,8 @@
+answer = input("What's my favorite food?")
+
+if answer == "Pizza":
+    print("Yup! So amazing!")  
+else:
+    print("Yuck! That's not it!")
+
+print("Thanks for playing!")
